ff5af3958e0ea5f3f3f4f37fcf3d10f0e00903b7
nhs219 <heesun4947@gmail.com>
2024-01-09 00:32:39 +0900

    chore: springsecurity 오류 못 찾겠어서 그냥 일단 permitAll()

--- src/main/java/com/routinise/config/SecurityConfig.java
+++ src/main/java/com/routinise/config/SecurityConfig.java
@@ -4,14 +4,15 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -20,16 +21,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf().disable()
-                .headers(headers -> headers.frameOptions().sameOrigin())	// H2 콘솔 사용을 위한 설정
-                .authorizeHttpRequests(requests ->
-                        requests.requestMatchers("/auth/**").permitAll()
-                                .requestMatchers(PathRequest.toH2Console()).permitAll()
-                                .anyRequest().authenticated()
-                )
+                .headers(headers -> headers.frameOptions().sameOrigin())
+                .authorizeHttpRequests()
+                    .requestMatchers(PathRequest.toH2Console()).permitAll()
+                    .anyRequest().permitAll()
+                .and()
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )	// 세션을 사용하지 않으므로 STATELESS 설정
+                )
+                .csrf().disable()
                 .build();
     }
 
