918d85912309b54f5c72f8b1d1f4a501fdbe71dc
nhs219 <heesun4947@gmail.com>
2024-01-15 21:09:50 +0900

    chore : 비밀번호 패턴 추가

--- src/main/java/com/routinise/request/Signup.java
+++ src/main/java/com/routinise/request/Signup.java
@@ -3,6 +3,7 @@ package com.routinise.request;
 import com.routinise.domain.Role;
 import com.routinise.domain.User;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,6 +18,8 @@ public class Signup {
     @NotBlank(message = "휴대폰 번호를 입력해주세요.")
     private String phone;
 
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d).{8,}$",
+    message = "영문과 숫자가 포함된 8자리 이상의 비밀번호를 입력해주세요.")
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
