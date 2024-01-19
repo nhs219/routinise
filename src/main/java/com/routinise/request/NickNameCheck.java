9a1986bd770d74309a6dcf844e1ba56d3fe999d8
nhs219 <heesun4947@gmail.com>
2024-01-19 16:40:28 +0900

    feat : nickname 중복 체크 api

---
+++ src/main/java/com/routinise/request/NickNameCheck.java
@@ -0,0 +1,12 @@
+package com.routinise.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class NickNameCheck {
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    private String nickName;
+}
